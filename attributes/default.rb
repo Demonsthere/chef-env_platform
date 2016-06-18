d6dae500f8b07f12cf57983a9bf421620cd5c0b0
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-06-18 16:37:46 +0200

    added qemu attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 # force_override attributes here
 force_override[:platform_apache][:port] = 4280
 force_override[:platform_qemu][:users] = 'jenkins'
+force_override[:platform_qemu][:home] = '/var/lib/jenkins'
